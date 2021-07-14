b3cc462fb806f0707f0385bd0f4ceedbeb37d839
DJSoftwarecreator <domi.jonas0910@gmail.com>
2021-07-14 11:15:55 +0200

    Asteroid neu

--- Asteroid.java
+++ Asteroid.java
@@ -1,33 +1,17 @@
 
-/**
- * Beschreiben Sie hier die Klasse Asteroid.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Asteroid
+
+public class Asteroid extends Actor
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
 
-    /**
-     * Konstruktor für Objekte der Klasse Asteroid
-     */
-    public Asteroid()
+    public Asteroid(int randomNumber)
     {
-        // Instanzvariable initialisieren
-        x = 0;
+    super("https://www.seekpng.com/ima/u2w7e6t4e6q8e6t4" );   
+     
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
+    public void act()
+  {
+    setDirection(90);
+    
+}
 }
