929bc2aa5e4cec488e1aa016aec635f7eb48a5ea
HFGq11projekt <67023817+HFGq11projekt@users.noreply.github.com>
2021-07-28 11:02:21 +0200

    Add files via upload
    Schuss mit verbesserter Asteroidenkollisionserkennung

--- Schuss.java
+++ Schuss.java
@@ -1,25 +1,50 @@
 import ch.aplu.jgamegrid.*;
-public class Schuss extends Actor
+public class Schuss extends Actor implements GGActorCollisionListener
 {
+    private boolean initialisiert;
+    
     public Schuss() {
         super ("img/schuss.png");
         setDirection(270);
+        initialisiert = false;
+        
     }
+    
+    public void initialisieren() {
+        // alle Asteroiden als Kollisionspartner anmelden
+        var alleAsteroiden = gameGrid.getActors(Asteroid.class);
+        alleAsteroiden.forEach(asteroid -> addCollisionActor(asteroid));
+        
+        addActorCollisionListener(this);
+    }
+
     public void act() {
+        if (!initialisiert) {
+            initialisieren();
+            initialisiert = true;
+        }
         move();
-        tryToEat();
-  }
 
-  private void tryToEat()
-  {
-    show(0);
-    Actor actor = gameGrid.getOneActorAt(getLocation(), Asteroid.class);
-    if (actor != null)
-    {
-      actor.removeSelf();
-      
     }
-  }
+    
+    
+    public int collide(Actor a1, Actor a2) {
+        a1.removeSelf();
+        a2.removeSelf();
+        
+        
+        return 10;
+    }
+    // private void tryToEat()
+    // {
+    // show(0);
+    // Actor actor = gameGrid.getOneActorAt(getLocation(), Asteroid.class);
+    // if (actor != null)
+    // {
+    // actor.removeSelf();
+
+    // }
+    // }
+    
+    
 }
-
-
